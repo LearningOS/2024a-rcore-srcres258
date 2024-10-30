d3a9411630d590306056eaa5a70b714c042df798
src_resources <src.res.211@gmail.com>
2024-10-30 22:45:14 +0800

    Finish sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -9,7 +9,9 @@ use crate::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next, TaskStatus,
     },
+    mm::copy_data_to_current_user
 };
+use crate::timer::get_time_us;
 
 #[repr(C)]
 #[derive(Debug)]
@@ -117,12 +119,20 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    trace!("kernel: sys_get_time");
+
+    // Create our version of TimeVal on the kernel stack.
+    let us = get_time_us();
+    let ts_kernel = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    // Then copy the TimeVal data from the kernel space to the user space.
+    copy_data_to_current_user(ts, &ts_kernel);
+
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
